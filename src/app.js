eac1ce3695046350522eeec4163f3039e2f2cc64
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-14 21:27:44 -0300

    feat: Make access to the post endpoint

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-console.log('app works');
+/*
+ * TODO: Make the Post request
+ *
+ * DB schema: { id: int, url: String }
+ *
+ *
+ * 1  - Recognize when the endpoint is accessed
+ * >>
+ * 1a - Create the response variable: {original_url: "", "short_url": ""}
+ * 2  - Parse/Sanitaze the payload
+ * 3  - Query the DB with the payload
+ * 4  - |IF| found
+ * 	a. assign the response variable with the DB data
+ * 5  - |ELSE|
+ * 	a. Save the url on the database
+ * 	b. assign the response variable with the DB data
+ * 6  - return the response variable to the client
+ * */
+
+/*
+ * TODO SWR
+ * */
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.post('/api/shorturl', (req, res) => {
+	res.send('ping /api/shorturl');
+});
+
+app.listen(port, () => console.log(`The app is listening on localhost:${port}`));

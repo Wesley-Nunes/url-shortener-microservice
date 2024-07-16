f7d43506ff3fafee6d44abe9282c5d028bf013f7
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-16 13:31:18 -0300

    feat: Add the body parser

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@
  *
  * DB schema: { id: int, url: String }
  *
- *
- * 1  - Recognize when the endpoint is accessed
- * >>
- * 1a - Create the response variable: {original_url: "", "short_url": ""}
- * 2  - Parse/Sanitaze the payload
+ * 2  - Parse/validateShorutl the payload
  * 3  - Query the DB with the payload
  * 4  - |IF| found
  * 	a. assign the response variable with the DB data
@@ -22,14 +18,23 @@
  * */
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import 'dotenv/config';
+import { validateShortUrl } from './validations.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.post('/api/shorturl', (req, res) => {
+app.post('/api/shorturl', validateShortUrl, (req, res) => {
+	const response = { original_url: "", short_url: "" };
+
+	console.log(req.body);
+
 	res.send('ping /api/shorturl');
 });
 
